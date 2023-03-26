9d2879d2e398af9eef9cc5c5c7fbe393cd5aef46
ryouhama <hary0730cap@icloud.com>
2023-03-26 19:45:17 +0900

    bettrt simple

--- create_zsh.py
+++ create_zsh.py
@@ -5,18 +5,22 @@ Usage:
     >>> poetry run python create_zsh.py
 """
 import getpass
-from services.alias_service import AliasService
+from loader import YamlLoader
+from models import Alias
 
 PATH = f"/Users/{getpass.getuser()}/.zshrc"
 YAML_PATH = "alias.yaml"
 
 
 def main():
-    alias_list = AliasService().fetch(YAML_PATH)
-    # TODO: print
+    loaded_alias = YamlLoader(YAML_PATH).load()
+
+    alias_items = [
+        Alias(name=key, command=value) for key, value in loaded_alias["alias"].items()
+    ]
     import pprint
 
-    pprint.pprint([it.to_string() for it in alias_list])
+    pprint.pprint([item.to_string() for item in alias_items])
 
 
 if __name__ == "__main__":
